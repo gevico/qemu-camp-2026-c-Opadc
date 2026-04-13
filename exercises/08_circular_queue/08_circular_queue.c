7f31333051f03f926102f064188eb80ec8652957
5d <d5@mypc.localdomain>
2026-04-13 21:39:22 +0800

    finish 08

--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -14,14 +14,60 @@ typedef struct {
     int count;
 } Queue;
 
+
+
+int queue_is_empty(Queue *q){
+    return q->count == 0;
+}
+void enqueue(Queue *q, People people){
+    q->data[q->tail] = people;
+    if(q->head == -1)
+        q->head = 0;
+    
+    q->tail = (q->tail+1) % MAX_PEOPLE;
+    q->count++;
+}
+
+People dequeue(Queue *q){
+    q->count--;
+    People p = q->data[q->head];
+    q->head = (q->head+1) % MAX_PEOPLE;
+    return p;
+    
+}
+
+void init_queue(Queue *q, int total_people){
+    q->tail = 0;
+    q->head = -1;
+    q->count = 0;
+    for(int i=1; i<=total_people; i++){
+        People people = {.id = i};
+        enqueue(q, people);
+    }
+}
+
 int main() {
     Queue q;
     int total_people=50;
     int report_interval=5;
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    init_queue(&q, total_people);
     
+    int curr_id = 1;
+    while(q.count > 1){
+        People people = dequeue(&q);
+        if(curr_id == report_interval){
+            printf("淘汰: %d\n", people.id);
+        }else{
+            enqueue(&q, people);
+        }
+        curr_id = curr_id + 1;
+        if(curr_id > report_interval){
+            curr_id = 1;
+        }
+
+    }
+
     printf("最后剩下的人是: %d\n", q.data[q.head].id);
 
     return 0;
