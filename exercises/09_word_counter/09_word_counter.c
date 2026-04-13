57c01509b71eebfcde78c4ebda927ac11f8cb5cc
5d <d5@mypc.localdomain>
2026-04-13 21:50:33 +0800

    finish 09

--- exercises/09_word_counter/09_word_counter.c
+++ exercises/09_word_counter/09_word_counter.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
-
+#include <ctype.h>
+#include <string.h>
 int main() {
     char str[]="Don't ask what your country can do for you, but ask what you can do for your country.";
     int wordCount = 0;
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    
+    char *p = str;
+    int in_word = 0;
+    while(*p){
+        // bypass all space.
+        while(*p && isspace(*p)){
+            if(in_word){
+                in_word = 0;
+                wordCount++;
+            }
+            p++;
+        }
+        while(*p && !isspace(*p)){
+            in_word = 1;
+            p++;
+        }
+    }
+
+    if(in_word){
+        wordCount++;
+    }
     
     printf("单词数量: %d\n", wordCount);
     
