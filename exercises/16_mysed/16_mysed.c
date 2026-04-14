c9efd6b40a792b64c483ca1c2b284e480a8c775a
5d <d5@mypc.localdomain>
2026-04-14 22:04:59 +0800

    finish 16

--- exercises/16_mysed/16_mysed.c
+++ exercises/16_mysed/16_mysed.c
@@ -9,15 +9,59 @@ int parse_replace_command(const char* cmd, char** old_str, char** new_str) {
         return -1;
     }
 
+    
+    char *begin_old;
+    char *end_old;
+    char *begin_new;
+    char *end_new;
+
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    while(*cmd && *cmd != '/'){
+        cmd++;
+    }
+    cmd++;
+    begin_old = cmd;
+    while(*cmd && *cmd != '/'){
+        cmd++;
+    }
+    end_old = cmd;
+
+    cmd++;
+    begin_new = cmd;
+    while(*cmd && *cmd != '/'){
+        cmd++;
+    }
+    end_new = cmd;
+
+    int old_len = end_old - begin_old;
+    int new_len = end_new - begin_new;
+
+    char *old_str_buf = malloc(old_len+1);
+    memcpy(old_str_buf, begin_old, old_len);
+    old_str_buf[old_len] = 0;
+
+    char *new_str_buf = malloc(new_len+1);
+    memcpy(new_str_buf, begin_new, new_len);
+    new_str_buf[new_len] = 0;
+
+    *old_str = old_str_buf;
+    *new_str = new_str_buf;
 
     return 0;
 }
 
 void replace_first_occurrence(char* str, const char* old, const char* new) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    char *fist_old = strstr(str, old);
+
+    int len = strlen(str);
+    int old_len = strlen(old);
+    int new_len = strlen(new);
+    //printf("%d %d \n", old_len, new_len);
+    memmove(fist_old+new_len, fist_old+old_len, len-old_len);
+    //printf("%s \n", str);
+    memcpy(fist_old, new, new_len);
+    str[len+new_len-old_len] = '\0';
 }
 
 int main(int argc, char* argv[]) {
