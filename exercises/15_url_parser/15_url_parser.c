2b6ceae41147a67ccaca55efdf8423e6a4a25fa5
5d <d5@mypc.localdomain>
2026-04-14 20:55:51 +0800

    finish 15

--- exercises/15_url_parser/15_url_parser.c
+++ exercises/15_url_parser/15_url_parser.c
@@ -3,19 +3,79 @@
 #include <string.h>
 #include <errno.h>
 
+enum {
+    WAIT_KEY,
+    READ_KEY,
+    WAIT_VALUE,
+    READ_VALUE,
+};
 /**
  * URL参数解析器
  * 输入：包含http/https超链接的字符串
  * 输出：解析出所有的key-value键值对，每行显示一个
  */
 
+
 int parse_url(const char* url) {
     int err = 0;
 
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    int state = WAIT_KEY;
+    char *p = (char *)url;
 
-exit:
+    char *key = NULL;
+    char *value = NULL;
+
+    char buf[50];
+
+    while(*p){
+        switch(state){
+            case(WAIT_KEY):
+                if(*p == '?'){
+                    state = READ_KEY;
+                }
+                break;
+            case(READ_KEY):
+                if(!key){
+                    key = p;
+                }
+                if(*p == '='){
+                    state = READ_VALUE;
+                    // print key
+                    int key_len = p-key;
+                    memcpy(buf, key, key_len);
+                    buf[key_len] = '\0';
+                    printf("key = %s, ", buf);
+                    key = NULL;
+                }
+                break;
+            case(READ_VALUE):
+                if(!value){
+                    value = p;
+                }
+                if(*p == '&'){
+                    state = READ_KEY;
+                    // print value
+                    int value_len = p-value;
+                    memcpy(buf, value, value_len);
+                    buf[value_len] = '\0';
+                    printf("value = %s\n", buf);
+
+                    value = NULL;
+                }
+                break;
+            
+        }
+        p++;
+    }
+    if(state == READ_VALUE){
+        int value_len = p-value;
+        memcpy(buf, value, value_len);
+        buf[value_len] = '\0';
+        printf("value = %s\n", buf);        
+    }
+
+
     return err;
 }
 
