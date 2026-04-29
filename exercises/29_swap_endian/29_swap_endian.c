62fddaf3f754675e38b1ea4c881a2f17ca0cafa6
5d <d5@mypc.localdomain>
2026-04-29 08:02:30 +0800

    finish 29

--- exercises/29_swap_endian/29_swap_endian.c
+++ exercises/29_swap_endian/29_swap_endian.c
@@ -3,7 +3,13 @@
 
 uint32_t swap_endian(uint32_t num) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    uint32_t byte0 = num & 0xff;
+    uint32_t byte1 = (num & 0xff00) >> 8;
+    uint32_t byte2 = (num & 0xff0000) >> 16;
+    uint32_t byte3 = (num & 0xff000000) >> 24;
+
+    printf("%x %x %x %x\n", byte0, byte1, byte2, byte3);
+    return (byte0 << 24) | (byte1 << 16) | (byte2 << 8) | byte3;
 }
 
 int main(int argc, char* argv[]) {
